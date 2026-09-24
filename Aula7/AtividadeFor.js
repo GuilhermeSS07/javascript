30b8d3a9dffba8b3ec12abe9bacf1685a3a14289
GuilhermeSS07 <gui2007.s@gmai.com>
2026-09-24 19:58:44 -0300

    commit atualizado

---
+++ Aula7/AtividadeFor.js
@@ -0,0 +1,12 @@
+var itens = ["maça", "leite", "café", "pão", "chocolate"]
+
+for(var i=0; i<itens.length; i++){
+
+    if(itens[i] === "café"){
+    console.log("Café disponível");
+    break;
+  }
+}
+
+
+
---
+++ Aula7/switch.js
@@ -0,0 +1,16 @@
+//For([inicialização]; [condição]; [ponto de parada]){
+    
+
+
+
+for(var i=0; i<=10; i++){
+   console.log(i)
+
+}
+
+
+var alunos = ["Ana", "Bruno", "Carlos", "Daniela", "Eduardo", "Fernanda", "Gabriel", "Helena", "Igor", "Julia"]
+
+for(var i=0; i<alunos.length; i++){
+    console.log("indice " + i + " : " + alunos[i]);
+}
